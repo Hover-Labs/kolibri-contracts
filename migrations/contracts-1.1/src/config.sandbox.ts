af406a8f5ada1be17d0ea2952f55a3dcd6b1798d
Keefer Taylor <keefer.t@gmail.com>
2021-09-21 23:12:30 -0700

    Deploy Scripts for v1.1 (#9)
    * wip
    
    * wip
    
    * more wip
    
    * work work
    
    * work in progress
    
    * end to end
    
    * cleanup files
    
    Co-authored-by: Keefer Taylor <keefer@keefertaylor.com>

---
+++ migrations/contracts-1.1/src/config.sandbox.ts
@@ -0,0 +1,53 @@
+// General config for a sandbox.
+import { TezosToolkit } from "@taquito/taquito";
+import { InMemorySigner } from "@taquito/signer";
+import { CONTRACTS, ContractGroup } from "@hover-labs/kolibri-js";
+import BigNumber from "bignumber.js";
+
+export const MANTISSA = Math.pow(10, 18)
+export const XTZ_MANTISSA = Math.pow(10, 6)
+
+export const LOG_LEVEL = 'info'
+
+// Network configuration
+export let NODE_URL = "https://sandbox.hover.engineering/"
+export let BETTER_CALL_DEV_BASE_URL = "https://bcd.hover.engineering/v1"
+export let OPERATION_DELAY_SECS = 10
+
+// Max retries for confirming transactions
+export const MAX_RETRIES_FOR_CONFIRMATION_POLLING = 10
+
+// Number of confirmations required
+export const NUMBER_OF_CONFIRMATIONS = 2
+
+// Contracts to use for Core.
+export const coreContracts: ContractGroup = CONTRACTS.SANDBOX
+
+// Governance parameters
+export const GOV_PARAMS = {
+    escrowAmount: new BigNumber('3000000000000000000000'),
+    voteLengthBlocks: new BigNumber('15'),
+    blocksInTimelockForExecution: new BigNumber('11')
+}
+
+// Deployer configuration
+export const privateKeyName = 'DEPLOY_SK'
+export const privateKey = process.env[privateKeyName] || ''
+if (privateKey === '') {
+    throw new Error(`Private key envar ${privateKeyName} must be set for operation!`)
+}
+
+// The location of the SmartPy CLI
+export const SMARTPY_CLI = '~/smartpy-cli/SmartPy.sh'
+
+// New Configuration
+export const PRIVATE_OWNER_LIQUIDATION_THRESHOLD = 180 * MANTISSA // 180%
+
+const Tezos = new TezosToolkit(NODE_URL);
+let initialized = false;
+export async function getTezos() {
+    if (!initialized) {
+        Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(privateKey) })
+    }
+    return Tezos
+}
---
+++ migrations/contracts-1.1/src/flows/migrate.ts
@@ -0,0 +1,71 @@
+import * as config from '../config'
+import crosscheck from '../crosscheck'
+import { generateBreakGlassStorage } from '../storage-wrappers/break-glass-contract'
+import { generateMinterStorage } from '../storage-wrappers/minter-contract'
+import { ContractOriginationResult, deployContract, fetchOrRun, sendOperation } from '../utils'
+
+const main = async () => {
+    console.log("Migrating contracts to 1.1")
+
+    // Step 0: Crosscheck and initialize
+    const { keystore, contractSources } = await crosscheck(config)
+
+    // Step 1: Deploy the new minter with governor set to deployer.
+    console.log("Formulating Storage from Old Minter")
+    console.log(`(Old Minter Contract: ${config.coreContracts.MINTER})`)
+    const minterDeployResult: ContractOriginationResult = await fetchOrRun(
+        'new-minter',
+        async () => {
+            const minterStorage = await generateMinterStorage(
+                config.coreContracts.MINTER!,
+                config.coreContracts.LIQUIDITY_POOL!,
+                keystore.publicKeyHash,
+                config.PRIVATE_OWNER_LIQUIDATION_THRESHOLD,
+                config
+            )
+            return deployContract(config, contractSources.newMinterContractSource, minterStorage)
+        }
+    )
+
+    // Step 2
+    console.log("Deploying a new Break Glass")
+    const breakGlassDeployResult: ContractOriginationResult = await fetchOrRun(
+        'new-minter-break-glass',
+        async () => {
+            const breakGlassStorage = generateBreakGlassStorage(
+                {
+                    daoAddress: config.coreContracts.DAO!,
+                    multisigAddress: config.coreContracts.BREAK_GLASS_MULTISIG!,
+                    targetAddress: minterDeployResult.contractAddress
+
+                }
+            )
+            return deployContract(config, contractSources.breakGlassContractSource, breakGlassStorage)
+        }
+    )
+
+    // Step 3: Wire the minter to accept commands from the break glass
+    console.log("Setting Minter to be governed by break glass")
+    const wireGovernorHash: string = await fetchOrRun(
+        'wire-governor',
+        async () => {
+            return sendOperation(config, minterDeployResult.contractAddress, 'setGovernorContract', breakGlassDeployResult.contractAddress)
+        }
+    )
+
+    // Step 4: Output Results
+    console.log("----------------------------------------------------------------------------")
+    console.log("Operation Results")
+    console.log("----------------------------------------------------------------------------")
+
+    console.log("Contracts:")
+    console.log(`New Minter Contract:        ${minterDeployResult.contractAddress} / ${minterDeployResult.operationHash}`)
+    console.log(`New Break Glass Contract:   ${breakGlassDeployResult.contractAddress} / ${breakGlassDeployResult.operationHash}`)
+    console.log("")
+
+    console.log("Operations:")
+    console.log(`Wire Minter To Break Glass: ${wireGovernorHash}`)
+    console.log("")
+}
+
+main()
