f092e6f0e0949a4a85730039ba448653eb54de97
Keefer Taylor <keefer@keefertaylor.com>
2021-10-06 21:20:48 -0400

    Remove smartpy out

--- smart_contracts/.smartpy_out/FA12_interpreted/testPrettyPrint.1.41.py
+++
@@ -1,9 +0,0 @@
-import smartpy as sp
-
-class Contract(sp.Contract):
-  def __init__(self):
-    self.init(last = sp.none)
-
-  @sp.entry_point
-  def target(self, params):
-    self.data.last = sp.some(params)
