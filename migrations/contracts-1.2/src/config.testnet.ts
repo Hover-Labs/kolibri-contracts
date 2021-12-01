572e0d224ec3a3694d0d1f050d511488a2455e5f
Keefer Taylor <keefer.t@gmail.com>
2021-12-01 14:06:50 -0800

    Update Contracts 1.2 Configurations on Test/Mainnet (#46)
    * update configs
    
    * update multisig
    
    * update runbooks for testnet
    
    * more runbook updates
    
    * update config for node
    
    * pull new kolibri-js
    
    * typo
    
    Co-authored-by: Keefer Taylor <keefer@keefertaylor.com>

--- migrations/contracts-1.2/src/config.testnet.ts
+++ migrations/contracts-1.2/src/config.testnet.ts
@@ -4,7 +4,7 @@ import BigNumber from "bignumber.js";
 
 export const NETWORK_CONFIG: NetworkConfig = {
   name: 'Granada Testnet',
-  tezosNodeUrl: 'https://granadanet.smartpy.io/chains/main/blocks/head/header',
+  tezosNodeUrl: 'https://rpctest.tzbeta.net',
   betterCallDevUrl: 'https://api.better-call.dev/v1',
   requiredConfirmations: 2,
   maxConfirmationPollingRetries: 10,
@@ -13,25 +13,23 @@ export const NETWORK_CONFIG: NetworkConfig = {
 
 export const KOLIBRI_CONFIG: KolibriConfig = {
   contracts: CONTRACTS.TEST,
-  escrowAmount: 1000000000000000000000,
-  governanceVoteLength: 10080,
-  governanceTimelockLength: 4320,
+  escrowAmount: 1,
+  governanceVoteLength: 10,
+  governanceTimelockLength: 6,
 }
 
 export const MIGRATION_CONFIG = {
   // Initial Interest Rate for Savings Pool 
-  // 7% Interest
-  // See https://www.wolframalpha.com/input/?i=ln%281+%2B+0.07%29+%2F+%2860+*+24+*+365%29
-  initialInterestRate: new BigNumber('128726500140'),
+  // 10% Interest
+  // See: https://www.wolframalpha.com/input/?i=%281+%2B+0.000000181335958532%29%5E%28365+*+24+*+60%29
+  initialInterestRate: new BigNumber('181335958532'),
 
   // Configuration for the Multisig that will become governor of the old stability fund
   stabilityFundMsig: {
-    threshold: 2,
+    threshold: 1,
     timelockSeconds: 0,
     publicKeys: [
-      'edpkuLh768382911CBbWkCN9joZkaZinKKeqPeMnxSoUb3X4TV7GpJ', // tz1YeYpdJshsXxkPdSdKUJaF1QmH1ngCrJ7V, Keefer
-      'edpktxA2V59rHy8FyHyfkiayz3y4cTYBcTWpooKJnREgBzJzLV7ZMT', // tz1Zygasw3bGh9uer7ue2KABjGFYpPsZVazZ, Luke
-      'edpkuPu3FQqWPFTXRT21BBCy5pstVoM6ynwzz9SaFnr6TVrg5Z7GrK', // tz1Xh11mHYWxbYHv55AVhsPPPJeSp8PunERB, Ryan
+      'edpkuLautNoSUnM3ynaYfkPgnJD9jLKj8VvFYieipoUYx3M5kjYenx', // tz1hover5EYqdWBtwxDfNVwQ1vvZ67u3ax1N, Hover Testnet Key, held by Keefer
     ],
   }
 }
